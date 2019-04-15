3c660ecd42503972c4c37e0b7ef9e137e7b78a70
Hex <hex@codeigniter.org.cn>
2019-04-15 19:37:43 +0800

    init commit

---
+++ src/extension.js
@@ -0,0 +1,17 @@
+// import * as vscode from 'vscode';
+
+import { NjwapExplorer } from './njwap'
+
+// let extensionContext;
+
+export function activate(context) {
+  // extensionContext = context;
+
+  new NjwapExplorer(context);
+
+	// vscode.commands.registerCommand('nodeDependencies.refreshEntry', () => nodeDependenciesProvider.refresh());
+	// vscode.commands.registerCommand('extension.openPackageOnNpm', moduleName => vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`https://www.npmjs.com/package/${moduleName}`)));
+	// vscode.commands.registerCommand('nodeDependencies.addEntry', () => vscode.window.showInformationMessage(`Successfully called add entry.`));
+	// vscode.commands.registerCommand('nodeDependencies.editEntry', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called edit entry on ${node.label}.`));
+	// vscode.commands.registerCommand('nodeDependencies.deleteEntry', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called delete entry on ${node.label}.`));
+}
